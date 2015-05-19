1b93295b1fcb388af9dc0970801263cc8fba916c
temp147 <temp147@163.com>
2015-05-19 18:03:33 +0800

    fix initial script

--- setup/createtable.sql
+++ setup/createtable.sql
@@ -1,27 +1,124 @@
-Create table if not exists sys_personbasicinfo(
-personid INT(10) not NULL AUTO_INCREMENT,
-lastname varchar(16),
-firstname varchar(16),
-pername varchar(64),
-phonenum varchar(16),
-password varchar(128),
-createtime datetime,
-creator varchar(40),
-modifytime datetime,
-modifier varchar(40),
-PRIMARY KEY(personid));
+drop table if exists sys_personbasicinfo;
 
-Create table if not exists pa_custemployee(
-empassignmentid INT(10) not NULL AUTO_INCREMENT,
-startdate date,
-enddate date,
-lastname varchar(16),
-firstname varchar(16),
-empname varchar(64),
-personid int,
-role smallint,
-createtime datetime,
-creator varchar(40),
-modifytime  datetime,
-modifier varchar(40),
-primary key (empassignmentid));
+/*==============================================================*/
+/* Table: sys_personbasicinfo                                   */
+/*==============================================================*/
+create table sys_personbasicinfo
+(
+   PersonID             int not null auto_increment,
+   LastName             varchar(16),
+   FirstName            varchar(16),
+   PerName              varchar(64),
+   PhoneNum             varchar(16),
+   PersonPass           varchar(128),
+   CreateTime           datetime,
+   Creator              varchar(40),
+   ModifyTime           datetime,
+   Modifier             varchar(40),
+   primary key (PersonID)
+);
+
+
+drop table if exists sys_mobiledeviceinfo;
+
+/*==============================================================*/
+/* Table: sys_mobiledeviceinfo                                  */
+/*==============================================================*/
+create table sys_mobiledeviceinfo
+(
+   DeviceCode           int not null auto_increment,
+   PersonID             int,
+   DeviceType           varchar(64),
+   SoftVersion          varchar(64),
+   CreateTime           datetime,
+   Creator              varchar(40),
+   ModifyTime           datetime,
+   Modifier             varchar(40),
+   primary key (DeviceCode)
+);
+
+drop table if exists sys_personcontactinfo;
+
+/*==============================================================*/
+/* Table: sys_personcontactinfo                                 */
+/*==============================================================*/
+create table sys_personcontactinfo
+(
+   PersonID             int not null,
+   QQ号码                 varchar(16),
+   EmailAddress         varchar(64),
+   CreateTime           datetime,
+   Creator              varchar(40),
+   ModifyTime           datetime,
+   Modifier             varchar(40),
+   primary key (PersonID)
+);
+
+drop table if exists pa_custemployee;
+/*==============================================================*/
+/* Table: pa_custemployee                                       */
+/*==============================================================*/
+create table pa_custemployee
+(
+   EmpID                int not null auto_increment,
+   TenantCode           varchar(8),
+   StartDate            date,
+   EndDate              date,
+   LastName             varchar(16),
+   FirstName            varchar(16),
+   EmpName              varchar(64),
+   PersonID             int,
+   CreateTime           datetime,
+   Creator              varchar(40),
+   ModifyTime           datetime,
+   Modifier             varchar(40),
+   primary key (EmpID)
+);
+
+
+drop table if exists pa_custemplink;
+
+/*==============================================================*/
+/* Table: pa_custemplink                                        */
+/*==============================================================*/
+create table pa_custemplink
+(
+   LinkID               int not null auto_increment,
+   EmpID                int not null,
+   TenantCode           varchar(8),
+   EntryLinkage         varchar(128),
+   StartDate            date,
+   EndDate              date,
+   CreateTime           datetime,
+   Creator              varchar(40),
+   ModifyTime           datetime,
+   Modifier             varchar(40),
+   primary key (LinkID)
+);
+
+drop table if exists om_empassignment;
+
+/*==============================================================*/
+/* Table: om_empassignment                                      */
+/*==============================================================*/
+create table om_empassignment
+(
+   EmpAssignmentID      int not null auto_increment,
+   StartDate            date,
+   EndDate              date,
+   CustID               varchar(8),
+   TenantCode           varchar(8),
+   EmpID                int,
+   OrgUnitID            int,
+   BizUnitiD            varchar(64),
+   IsMainAssign         char(0),
+   IsOrgManager         char(0),
+   AssignIndex          smallint,
+   IsExpatriate         char(0),
+   Status               varchar(16),
+   CreateTime           datetime,
+   Creator              varchar(40),
+   ModifyTime           datetime,
+   Modifier             varchar(40),
+   primary key (EmpAssignmentID)
+);
